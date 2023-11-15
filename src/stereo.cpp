055747bca9517fbc3774ff61b28eb9097549e221
4399chen <56430444+4399chen@users.noreply.github.com>
2023-11-15 15:57:07 +0800

    Update stereo.cpp
    使用v4l2作为opencv后端，提高fps

--- src/stereo.cpp
+++ src/stereo.cpp
@@ -22,27 +22,27 @@ using namespace cv;
 sensor_msgs::CameraInfo getCameraInfoL(void){        // extract cameraInfo.
     sensor_msgs::CameraInfo cam;
 
-    vector<double> D{-0.020917, 0.048395, -0.002389, 0.000589, 0.000000};
+    vector<double> D{-0.170127, 0.024090, 0.000306, 0.001427, 0.000000};
 
     boost::array<double, 9> K = {
-        867.011713, 0.000000, 695.941232,
-        0.000000, 867.816427, 412.125027,
+        691.146247, 0.000000, 640.581338,
+        0.000000, 691.905303, 368.062655,
         0.000000, 0.000000, 1.000000
     };
 
     // Mat p = getOptimalNewCameraMatrix(k, d, Size(1280, 720), 0);        // get rectified projection.
     
     boost::array<double, 12> P = {
-        960.583503, 0.000000, 732.428703, 0.000000,
-        0.000000, 960.583503, 426.645927, 0.000000,
+        684.535486, 0.000000, 682.176537, 0.000000,
+        0.000000, 684.535486, 355.036983, 0.000000,
         0.000000, 0.000000, 1.000000, 0.000000
 		
     };
 
     boost::array<double, 9> r = {
-        0.999733, 0.003436, -0.022859,
-        -0.003437, 0.999994, -0.000029,
-        0.022859, 0.000108, 0.999739
+        0.999888, 0.001631, -0.014880,
+        -0.001612, 0.999998, 0.001261,
+        0.014882, -0.001236, 0.999888
 	};
 
     cam.height = 720;
@@ -62,31 +62,29 @@ sensor_msgs::CameraInfo getCameraInfoL(void){        // extract cameraInfo.
     return cam;
 }
 
- 
 sensor_msgs::CameraInfo getCameraInfoR(void){        // extract cameraInfo.
     sensor_msgs::CameraInfo cam;
 
-    vector<double> D{0.009107, 0.006306, 0.000341, 0.001296, 0.000000};
+    vector<double> D{-0.168092, 0.019713, -0.000505, 0.000931, 0.000000};
 
     boost::array<double, 9> K = {
-        863.730103, 0.000000, 707.376905,
-        0.000000, 863.973069, 445.883640,
+        695.453181, 0.000000, 647.137479,
+        0.000000, 696.220713, 346.575235,
         0.000000, 0.000000, 1.000000
     };
 
     // Mat p = getOptimalNewCameraMatrix(k, d, Size(1280, 720), 0);        // get rectified projection.
     
     boost::array<double, 12> P = {
-        960.583503, 0.000000, 732.428703, -133.457400,
-        0.000000, 960.583503, 426.645927, 0.000000,
+        684.535486, 0.000000, 682.176537, -43.371869,
+        0.000000, 684.535486, 355.036983, 0.000000,
         0.000000, 0.000000, 1.000000, 0.000000
-		
     };
 
     boost::array<double, 9> r = {
-        0.999692, 0.004935, -0.024333,
-        -0.004933, 0.999988, 0.000129,
-        0.024334, -0.000009, 0.999704
+        0.999910, 0.001206, -0.013360,
+        -0.001223, 0.999998, -0.001240,
+        0.013359, 0.001257, 0.999910
 	};
 
     cam.height = 720;
@@ -106,7 +104,6 @@ sensor_msgs::CameraInfo getCameraInfoR(void){        // extract cameraInfo.
     return cam;
 }
 
-
 int main(int argc,char** argv)
 {
     //初始化ROS，节点命名为image_publisher，节点命名必须保持唯一
@@ -124,14 +121,14 @@ int main(int argc,char** argv)
     sensor_msgs::CameraInfo camera_info_L;
     sensor_msgs::CameraInfo camera_info_R;
 
-
-    
     cv::Mat stereo_image,imLeft,imRight;
     int width=2560;
     int height=720;
     int fps=60;
  
-    cv::VideoCapture cap(0);
+    // cv::VideoCapture cap(0);
+    cv::VideoCapture cap(0 + cv::CAP_V4L2);
+
     //分辨率
     cap.set(cv::CAP_PROP_FRAME_WIDTH,width);
     cap.set(cv::CAP_PROP_FRAME_HEIGHT,height);
@@ -139,34 +136,37 @@ int main(int argc,char** argv)
     cap.set(cv::CAP_PROP_FPS,fps);
     //视频流格式
     cap.set(cv::CAP_PROP_FOURCC,cv::VideoWriter::fourcc('Y','U','Y','2'));
- 
+    // cap.set(cv::CAP_PROP_FOURCC,cv::VideoWriter::fourcc('M','J','P','G'));
+
+    // 设置曝光时间
+    double desired_exposure_time = 1;        // min=1 max=80000 step=1 default=312 value=65536 flags=inactive
+    cap.set(cv::CAP_PROP_AUTO_EXPOSURE, 3);     // 1: Manual Mode, 3: Aperture Priority Mode
+    cap.set(cv::CAP_PROP_EXPOSURE, desired_exposure_time);
+
+    if (!cap.isOpened()) {
+        cerr << "Error opening video stream" << endl;
+        return -1;
+    }
+
     std::cout<<"开始发布图像数据..."<<std::endl;
     std::cout<<"分辨率:"<<width*0.5<<"*"<<height<<std::endl;    
-    std::cout<<"帧率:"<<fps<<"fps"<<std::endl;    
+    std::cout<<"帧率:"<<fps<<"fps"<<std::endl;   
+
+    //设置发布数据的频率
+    ros::Rate loop_rate(fps); 
  
     while(n.ok())
     {
         //获取图像
         cap >> stereo_image;
-        //分割成左右目
-        /*
-        rowRange为指定的行span创建一个新的矩阵头，可取指定行区间元素
-        colRange为指定的列span创建一个新的矩阵头，可取指定列区间元素
-        分割图像是取宽的一半，所以指定0-width/2的列区间
-        不clone会对元矩阵直接操作
-        */
-        imLeft=stereo_image.colRange(0,width/2).clone();
-        imRight=stereo_image.colRange(width/2,width).clone();
-        //转成灰度图
-        // cv::cvtColor(imLeft,imLeft,cv::COLOR_BGR2GRAY);
-        // cv::cvtColor(imRight,imRight,cv::COLOR_BGR2GRAY);
-        //将图像类型转化成ROS消息类型
-        // sensor_msgs::ImagePtr msg_L = cv_bridge::CvImage(std_msgs::Header(), "mono8", imLeft).toImageMsg();
-        // sensor_msgs::ImagePtr msg_R = cv_bridge::CvImage(std_msgs::Header(), "mono8", imRight).toImageMsg();
+
+        //分割成左右目，避免使用.clone()
+        imLeft = stereo_image(cv::Rect(0, 0, width/2, height));
+        imRight = stereo_image(cv::Rect(width/2, 0, width/2, height));
+
         sensor_msgs::ImagePtr msg_L = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imLeft).toImageMsg();
         sensor_msgs::ImagePtr msg_R = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imRight).toImageMsg();
-        //设置发布数据的频率
-        ros::Rate loop_rate(fps);
+        
         //读取当前时间作为图像的时间戳
         camera_info_L = getCameraInfoL();
         camera_info_R = getCameraInfoR();
@@ -181,7 +181,8 @@ int main(int argc,char** argv)
         pubLi.publish(camera_info_L);
         PubR.publish(msg_R);
         pubRi.publish(camera_info_R);
+
         //按照前面设置的频率将程序挂起
-        loop_rate.sleep();
+        // loop_rate.sleep();
     }
 }
